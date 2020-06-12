14686c2e70039a36f51430863a1dd129a95c34a1
Nyannazz <kantemir.imam@gmail.com>
2020-06-12 19:10:18 +0200

    asd

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c733a7e72f1f8395f607c6ce32b32de.js"
+  "/precache-manifest.056e6a4e3a4a1a8d0608607a4eded330.js"
 );
 
 workbox.clientsClaim();
